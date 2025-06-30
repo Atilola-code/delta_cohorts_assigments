e12adc92a21e58e6ea6a7ab55c4ddf0ec5fc51f3
Atilola-code <sannimistura1211@gmail.com>
2025-06-30 09:28:43 +0100

    feat: assignments on the last class

---
+++ week_one/assignment.py
@@ -0,0 +1,65 @@
+#Assignments
+
+#number 1
+
+full_name = "Chuka Emeka"
+
+full_name = "Chuka Okeke"
+
+print("after name update:", full_name)
+
+# number 2
+
+height = 1.75
+age = 27
+print(f"my height is {height} and my age is {age}")
+
+#number 3
+
+is_logged_in = True
+print(is_logged_in)
+
+#number 4
+
+first ="Ada"
+last = "Okoro"
+print(f"The first and last name is: {first + " " + last}")
+
+#number 5
+
+num1 = 29
+num2 = 5
+is_remainder = num1 % num2
+print(f"The remainer is: {is_remainder}")
+
+#number 6
+
+val1 = input("What is your first value:")
+val2 = input("What is your second value:")
+conversion = float(val1) + float(val2)
+print(f"The total value is:", conversion)
+ 
+ #number 7
+
+val3 = 7
+val4 = 5
+compare = val3 >= val4
+print(f"The comparison result: {compare}")
+
+#number 8
+
+has_id = True
+has_clearance = True
+comparebool = has_id and has_clearance
+print("He has id and has clearance :", comparebool)
+
+#number 9
+has_pass = False
+knows_manager = True
+isTrue = has_pass or knows_manager
+print(f"He pass or knows manager: {isTrue}")
+
+#number 10
+can_enter = False
+compare2 = not can_enter
+print(f"He can enter: {compare2}")
